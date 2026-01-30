5af04ecd827c8eb9e81d935380b90d688c3dc911
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-30 15:42:48 -0500

    update pokerHand

--- PokerDetect/pokerHand.py
+++ PokerDetect/pokerHand.py
@@ -10,9 +10,29 @@ def find_poker_hand(hand):
         else:
             rank = card[0:2]
             suit = card[2]
+        if rank == 'A':
+            rank = 14
+        elif rank == 'K':
+            rank = 13
+        elif rank == 'Q':
+            rank = 12
+        elif rank == 'J':
+            rank = 11
+        else: rank = int(rank)  # handles 2–10
+
         print(rank, suit)
         ranks.append(rank)
         suits.append(suit)
+
+    sorted_ranks = sorted(ranks)
+    print(sorted_ranks)
+
+    # Royal Flush
+    if suits.count(suits[0]) == 5:
+        print("Flush")
+    else:
+        print("Mix")
+
 
     print(hand)
     return 0
