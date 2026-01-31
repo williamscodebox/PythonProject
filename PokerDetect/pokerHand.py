252ccb6c52fbca088847acc155407832ae72582c
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-31 13:29:53 -0500

    update pokerHand

--- PokerDetect/pokerHand.py
+++ PokerDetect/pokerHand.py
@@ -1,7 +1,13 @@
 def find_poker_hand(hand):
 
+    poker_hand_ranks = {10: "Royal Flush", 9: "Straight Flush", 8: "Four Of A Kind", 7: "Full House", 6: "Flush",
+                        5: "Straight", 4: "Three Of A Kind", 3: "Two Pairs", 2: "One Pair", 1: "High Card",
+                        0: "No Hand Currently Defined in Function Definition"}
     ranks = []
     suits = []
+    possible_ranks = []
+
+    print("")
 
     for card in hand:
         if len(card) ==2:
@@ -25,16 +31,21 @@ def find_poker_hand(hand):
         suits.append(suit)
 
     sorted_ranks = sorted(ranks)
-    print(sorted_ranks)
+    # print(sorted_ranks)
 
     # Royal Flush
     if suits.count(suits[0]) == 5:
-        print("Flush")
+        if 14 in sorted_ranks and 13 in sorted_ranks and 12 in sorted_ranks and 11 in sorted_ranks and 10 in sorted_ranks:
+            # print("Royal Flush")
+            possible_ranks.append(10)
+        # print("Flush")
+        possible_ranks.append(0)
     else:
-        print("Mix")
+        print("")
+        # print("Mix")
 
-
-    print(hand)
+    # print(hand)
+    print(poker_hand_ranks[max(possible_ranks)])
     return 0
 
 if __name__ == '__main__':
